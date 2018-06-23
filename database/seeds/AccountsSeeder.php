bdee2cb942a815dd24eb53886a091401e7c3890a
Matthew Hotchen <m@mhn.me>
2018-06-23 13:38:20 +0100

    Fill measurement settings during seeding

--- database/seeds/AccountsSeeder.php
+++ database/seeds/AccountsSeeder.php
@@ -1,13 +1,25 @@
 <?php
 
+use App\Measurement\Settings\DefaultMeasurementSetting;
+use App\Measurement\Settings\DefaultMeasurementSettings;
 use App\Models\Company;
 use App\Models\Customer;
 use App\Models\CustomerNote;
+use App\Models\MeasurementSetting;
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Collection;
 
 class AccountsSeeder extends Seeder
 {
+    /** @var DefaultMeasurementSettings */
+    private $defaultMeasurementSettings;
+
+    public function __construct(DefaultMeasurementSettings $defaultMeasurementSettings)
+    {
+        $this->defaultMeasurementSettings = $defaultMeasurementSettings;
+    }
+
     /**
      * Run the database seeds.
      *
@@ -16,15 +28,26 @@ class AccountsSeeder extends Seeder
     public function run()
     {
         /*
-         * Expect this to take a while: 500 users, ~500,000 customers, ~2.5 million notes.
+         * Expect this to take a while: 500 users/companies, ~500,000 customers, ~2.5 million notes.
          *
          * This means during development missing indexes will be obvious.
          */
         factory(User::class, 500)->make()->each(function (User $user, $i): void {
             $user->email = "test$i@tailoring.io";
             $user->save();
+
+            /** @var Company $company */
             $company = factory(Company::class)->make();
             $user->companies()->save($company);
+
+            $company->measurementSettings()->saveMany(
+                (new Collection($this->defaultMeasurementSettings->getSettings()))
+                    ->map(function (DefaultMeasurementSetting $default) use ($user) {
+                        $setting = new MeasurementSetting;
+                        $setting->fillFromDefault($default, $user);
+                        return $setting;
+                    })
+            );
 
             factory(Customer::class, random_int(900, 1000))
                 ->make()
